34221cd3c4ec2c080f55ad13e08c94753ea399e4
Bolat Tussupzhanov <tussupzhanov@gmail.com>
2016-08-02 12:18:00 +0000

    timestamp ready!

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var strftime = require('strftime');
 var app = express();
 
+
+app.get('/:tuktuk', function (req, res) {
+    var ddd = Date.parse(req.params.tuktuk);
+    if (isNaN(ddd)){ 
+        res.end('null');
+    }else{
+        var dt = new Date(ddd);
+       var ob = {
+         "unit":dt.getTime(),
+         "natural": strftime('%B %d, %Y',dt)
+       };
+        res.end(JSON.stringify(ob));
+    }
+  // res.end(JSON.stringify(ob));    
+    
+  //res.send('Hello World! '+req.params.tuktuk);
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('This is timestamp service for FreeCodeCamp challenge!');
 });
 
 app.listen(8080, function () {
